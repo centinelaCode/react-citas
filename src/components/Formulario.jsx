93778139580468d090ff877a973c4166b5d3dc14
centinela1024tb <centinela1024tb@gmail.com>
2022-06-17 17:17:18 -0500

    [ADD] Se agrega el state y evento onClick para poder capturar desde el input

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,17 @@
+import { useState, useEffect } from 'react'
 
 const Formulario = () => {
+
+  // States
+  const [nombre, setNombre] = useState(''); 
+
+
+  // Funcitios
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Enviando Formulario')
+  }
+
 
   return (
     <div className="md:w-1/2 lg:w-2/5">
@@ -10,19 +22,24 @@ const Formulario = () => {
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form 
+        onSubmit={ handleSubmit }
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
         <div className="mb-5">
           <label 
             htmlFor="mascota"
             className="block text-gray-700 uppercase font-bold"
           >
-            Nombre Mascota
+            Nombre Mascota { nombre }
           </label>
           <input 
             id="mascota"
             type="text"
             placeholder="Nombre de la mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gary-400 rounded-md"
+            value={nombre}
+            onChange={ (e) => setNombre(e.target.value) }
           />
         </div>
 
