f6875ac3dfc2bd113ad797e5482f84678d11b525
centinela1024tb <centinela1024tb@gmail.com>
2022-06-17 19:30:01 -0500

    [ADD] Se pasa el mensaje de error por medio de children
    Para hacer uso de children se debe usar el componente de la forma <componente> los children  </componente>

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -60,7 +60,7 @@ const Formulario = ({ pacientes, setPacientes }) => {
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
 
-        {error && <Error mensaje='Todos los campos son obligatorios' />}
+        {error && <Error><p>Todos los campos son obligatorios</p></Error>}
 
         <div className="mb-5">
           <label 
