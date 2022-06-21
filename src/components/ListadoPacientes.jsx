467b25855e9ec2fbcdbc6053073be08bd433a2c1
centinela1024tb <centinela1024tb@gmail.com>
2022-06-21 17:46:41 -0500

    [ADD] Se muestra de forma condicional si hay pacientes y si no muestra que no hay pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,24 +2,41 @@ import Paciente from './Paciente';
 
 const ListadoPacientes = ({pacientes}) => {
   
-  console.log(pacientes)
+  // console.log(pacientes.length === 0);
+  // console.log(pacientes && pacientes.length);
 
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2> 
-      <p className="text-xl mt-5 mb-5 text-center">
-        Administra tus {''}
-        <span className="text-indigo-600 font-bold text">Pacientes y Citas</span>
-      </p>
 
-      {
-        pacientes.map( paciente => (
-          <Paciente 
-            key={paciente.id}
-            paciente={paciente}
-          />      
-        ))
-      }
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2> 
+          <p className="text-xl mt-5 mb-5 text-center">
+            Administra tus {''}
+            <span className="text-indigo-600 font-bold text">Pacientes y Citas</span>
+          </p>
+    
+          {
+            pacientes.map( paciente => (
+              <Paciente 
+                key={paciente.id}
+                paciente={paciente}
+              />      
+            ))
+          }        
+        </>
+
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay Pacientes</h2> 
+          <p className="text-xl mt-5 mb-5 text-center">
+            Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold text">y apareceran en este lugar</span>
+          </p>          
+        </>
+
+      )}
+
       
     </div> 
   )
